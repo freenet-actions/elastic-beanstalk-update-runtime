9a80e2d9d70c50efb73adb1f6415b78975ab070c
sgoral <Sebastian.Goral@md.de>
2023-01-19 17:46:26 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,97 @@
+const core = require('@actions/core');
+const {ElasticBeanstalkClient, DescribeEnvironmentsCommand, ListAvailableSolutionStacksCommand, UpdateEnvironmentCommand} = require('@aws-sdk/client-elastic-beanstalk');
+
+async function getCurrentSolutionStackName(client, applicationName, environmentName) {
+  const command = new DescribeEnvironmentsCommand({
+    ApplicationName: applicationName,
+    EnvironmentNames: [environmentName]
+  });
+  const response = await client.send(command);
+  if (response.Environments.length === 0) {
+    core.setFailed(`No environment "${environmentName}" in application "${applicationName}" found.`);
+    process.exit(1);
+  } else if (response.Environments.length !== 1) {
+    core.setFailed(`Found ${response.Environments.length} environments with name "${environmentName}" in application "${applicationName}".`);
+    process.exit(2);
+  } else {
+    const currentSolutionStack = response.Environments[0].SolutionStackName;
+    if (currentSolutionStack === undefined) {
+      core.setFailed(`Environment "${environmentName}" in application "${applicationName}" does not have a solution stack name.`);
+      process.exit(3);
+    } else {
+      return currentSolutionStack;
+    }
+  }
+}
+
+function compareSolutionStackName(actual, expected, useRegex) {
+  if (useRegex) {
+    return actual.match(expected) !== null;
+  } else {
+    return expected === actual;
+  }
+}
+
+async function fetchLatestAvailableSolutionStack(client, expected) {
+  const regex = new RegExp(expected);
+  const command = new ListAvailableSolutionStacksCommand();
+  const response = await client.send(command);
+  for(const solution of response.SolutionStacks) {
+    if (regex.test(solution)) {
+      return solution;
+    }
+  }
+
+  core.info('The following solution stacks were found:');
+  for(const solution of response.SolutionStacks) {
+    core.info(`- ${solution}`);
+  }
+  core.setFailed(`No solution stack with a name matching "${expected}" found.`);
+  process.exit(4);
+}
+
+async function triggerPlatformUpdate(client, applicationName, environmentName, solutionStackName) {
+  const command = new UpdateEnvironmentCommand({
+    ApplicationName: applicationName,
+    EnvironmentName: environmentName,
+    SolutionStackName: solutionStackName
+  });
+
+  await client.send(command);
+}
+
+(async () => {
+  try {
+    const awsAccessKey = core.getInput('aws_access_key', {required: true});
+    const awsSecretKey = core.getInput('aws_secret_key', {required: true});
+    const applicationName = core.getInput('application_name', {required: true});
+    const environmentName = core.getInput('environment_name', {required: true});
+    const region = core.getInput('region', {required: true});
+    const expected = core.getInput('expected', {required: true});
+    const matchRegex = core.getBooleanInput('match_regex');
+
+    const client = new ElasticBeanstalkClient({
+      credentials: {
+        accessKeyId: awsAccessKey,
+        secretAccessKey: awsSecretKey,
+      },
+      region: region
+    });
+
+    const currentSolutionStackName = await getCurrentSolutionStackName(client, applicationName, environmentName);
+    core.info(`Environment "${environmentName}" in application "${applicationName}" is currently running solution stack "${currentSolutionStackName}".`);
+    if (compareSolutionStackName(currentSolutionStackName, expected, matchRegex)) {
+      core.info('Solution stack name matches expectation, no update required.');
+      process.exit(0);
+    }
+
+    const target = matchRegex ? await fetchLatestAvailableSolutionStack(client, expected) : expected;
+    core.info(`Updating environment "${environmentName}" in application "${applicationName}" to solution stack "${target}".`);
+    await triggerPlatformUpdate(client, applicationName, environmentName, target);
+    core.info('Update triggered.');
+
+    process.exit(0);
+  } catch(error) {
+    core.setFailed(error.message);
+  }
+})();
---
+++ readme.md
@@ -0,0 +1,59 @@
+# elastic-beanstalk-update-runtime
+
+This action updates the solution stack used by an AWS Elastic Beanstalk environment.
+
+## Usage
+
+### Exact matching
+
+You can specify an exact solution stack name to match against:
+
+```yaml
+- uses: freenet-actions/elastic-beanstalk-update-runtime@HEAD
+  with:
+    aws_access_key: ${{ secrets.AWS_ACCESS_KEY_ID }}
+    aws_secret_key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+    application_name: MyApplicationName
+    environment_name: MyApplication-Environment
+    region: eu-central-1
+    expected: 64bit Amazon Linux 2 v3.5.3 running PHP 8.1
+```
+
+The action will trigger an update when the solution stack of the environment does
+not match the expectation exactly.
+
+### Regex matching
+
+You can also enable regex matching:
+
+```yaml
+- uses: freenet-actions/elastic-beanstalk-update-runtime@HEAD
+  with:
+    aws_access_key: ${{ secrets.AWS_ACCESS_KEY_ID }}
+    aws_secret_key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+    application_name: MyApplicationName
+    environment_name: MyApplication-Environment
+    region: eu-central-1
+    expected: 64bit Amazon Linux 2 v.* running PHP 8.1
+    match_regex: true
+```
+
+The action will then trigger an update when the solution stack of the environment does
+not match the `expectated` regex. The new solution stack will be the latest solution stack
+that matches the `expected` regex.
+
+## Inputs
+
+| Input              | Required | Description                                    |
+|--------------------|----------|------------------------------------------------|
+| `aws_access_key`   | yes      | AWS credentials                                |
+| `aws_secret_key`   | yes      | AWS credentials                                |
+| `application_name` | yes      | The application to update                      |
+| `environment_name` | yes      | The environment to update                      |
+| `region`           | yes      | The region in which the application is running |
+| `expected`         | yes      | The expected solution stack name               |
+| `match_regex`      | no       | Set to true to enable regex matching.          |
+
+## Development
+
+For GitHub actions, all dependencies need to be included in the repository. Therefore, node_modules is not .gitignore'd.
